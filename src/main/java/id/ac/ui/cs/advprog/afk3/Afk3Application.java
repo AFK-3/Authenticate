d441c6f155af86e847936317603e1b1434626e1d
KenKomKom <kenichikomala@gmail.com>
2024-04-23 21:57:07 +0700

    impleemnt for changes to gcp-sell api

--- src/main/java/id/ac/ui/cs/advprog/afk3/Afk3Application.java
+++ src/main/java/id/ac/ui/cs/advprog/afk3/Afk3Application.java
@@ -13,51 +13,54 @@ import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFuncti
 @SpringBootApplication
 @EnableConfigurationProperties
 public class Afk3Application {
+
+	String url_gcp_sell = "http://34.126.165.220";
+	String url_local = "http://localhost:8081/";
 	public static void main(String[] args) {
 		SpringApplication.run(Afk3Application.class, args);
 	}
 	@Bean
 	public RouterFunction<ServerResponse> toUserSellerList() {
-		return route("userSeller/list").GET("/userSeller/list", http("http://34.126.165.220")).build();
+		return route("userSeller/list").GET("/userSeller/list", http(url_gcp_sell)).build();
 	}
 	@Bean
 	public RouterFunction<ServerResponse> toListingList() {
-		return route("listing/list").GET("/listing/list", http("http://34.126.165.220")).build();
+		return route("listing/list").GET("/listing/list", http(url_gcp_sell)).build();
 	}
 	@Bean
 	public RouterFunction<ServerResponse> toUserSellerCreate() {
-		return route("userSeller/create").GET("/userSeller/create", http("http://34.126.165.220")).build();
+		return route("userSeller/create").GET("/userSeller/create", http(url_gcp_sell)).build();
 	}
 	@Bean
 	public RouterFunction<ServerResponse> toPostUserSellerCreate() {
-		return route("userSeller/create").POST("/userSeller/create", http("http://34.126.165.220")).build();
+		return route("userSeller/create").POST("/userSeller/create", http(url_gcp_sell)).build();
 	}
 	@Bean
 	public RouterFunction<ServerResponse> toPostUserSellerEdit() {
-		return route("userSeller/create").POST("/userSeller/edit", http("http://34.126.165.220")).build();
+		return route("userSeller/create").POST("/userSeller/edit", http(url_gcp_sell)).build();
 	}
 	@Bean
 	public RouterFunction<ServerResponse> toPostListingCreate() {
-		return route("listing/create").POST("/listing/create", http("http://34.126.165.220")).build();
+		return route("listing/create").POST("/listing/create", http(url_gcp_sell)).build();
 	}
 	@Bean
 	public RouterFunction<ServerResponse> toPostListingEdit() {
-		return route("listing/edit").POST("/listing/edit", http("http://34.126.165.220")).build();
+		return route("listing/edit").POST("/listing/edit", http(url_gcp_sell)).build();
 	}
 	@Bean
 	public RouterFunction<ServerResponse> toPostListingDelete() {
-		return route("listing/delete").POST("/listing/delete", http("http://34.126.165.220")).build();
+		return route("listing/delete").POST("/listing/delete", http(url_gcp_sell)).build();
 	}
 	@Bean
 	public RouterFunction<ServerResponse> toPostOrderCreate() {
-		return route("order/create").POST("/order/create", http("http://34.126.165.220")).build();
+		return route("order/create").POST("/order/create", http(url_gcp_sell)).build();
 	}
 	@Bean
 	public RouterFunction<ServerResponse> toPostOrderToSeller() {
-		return route("order/to-seller").POST("/order/to-seller", http("http://34.126.165.220")).build();
+		return route("order/to-seller").POST("/order/to-seller", http(url_gcp_sell)).build();
 	}
 	@Bean
 	public RouterFunction<ServerResponse> toPostOrderSetStatus() {
-		return route("order/set-status/**").POST("/order/set-status/**", http("http://34.126.165.220")).build();
+		return route("order/set-status/**").POST("/order/set-status/**", http(url_gcp_sell)).build();
 	}
 }
