f9a34b9642a3f8e541a7b7c478ab43ebc1d62f6c
KenKomKom <119410845+KenKomKom@users.noreply.github.com>
2024-04-29 23:59:39 +0700

    Update Afk3Application.java

--- src/main/java/id/ac/ui/cs/advprog/afk3/Afk3Application.java
+++ src/main/java/id/ac/ui/cs/advprog/afk3/Afk3Application.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -13,6 +14,7 @@ import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouter
 import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
 
 @SpringBootApplication
+@Component
 @ConfigurationProperties(prefix = "app")
 public class Afk3Application {
 
