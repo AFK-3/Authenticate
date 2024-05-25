cfce9d748baa9a0050cc5a8b934d518a70b73252
Farrell Muhammad Hanau <hanau.muhammad@gmail.com>
2024-05-26 02:04:18 +0700

    remove unused import

--- src/main/java/id/ac/ui/cs/advprog/afk3/Afk3Application.java
+++ src/main/java/id/ac/ui/cs/advprog/afk3/Afk3Application.java
@@ -1,6 +1,5 @@
 package id.ac.ui.cs.advprog.afk3;
 
-import org.apache.catalina.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
